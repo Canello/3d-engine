a685da01f55dc7d13142ab1fc3d8686c50960180
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-08-31 10:34:22 -0300

    feat: add movement methods to camera

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,59 @@
 // NEXT STEPS
-// 1) Account for movable camara
+// 1) Listen to keyboard
+// 2) Add animation to camera movement
 
 class Point {
     constructor(public x: number, public y: number, public z: number) {}
+
+    scale(s: number) {
+        return new Point(this.x * s, this.y * s, this.z * s);
+    }
+
+    rotate(axis: "x" | "y" | "z", angle: number) {
+        const radians = (angle * Math.PI) / 180;
+        const cos = +Math.cos(radians).toFixed(5);
+        const sin = +Math.sin(radians).toFixed(5);
+
+        if (axis === "x") {
+            const y = cos * this.y - sin * this.z;
+            const z = sin * this.y + cos * this.z;
+            return new Point(this.x, y, z);
+        } else if (axis === "y") {
+            const x = cos * this.x + sin * this.z;
+            const z = -sin * this.x + cos * this.z;
+            return new Point(x, this.y, z);
+        } else {
+            const x = cos * this.x - sin * this.y;
+            const y = sin * this.x + cos * this.y;
+            return new Point(x, y, this.z);
+        }
+    }
+
+    add(v: Point) {
+        const x = this.x + v.x;
+        const y = this.y + v.y;
+        const z = this.z + v.z;
+        return new Point(x, y, z);
+    }
+
+    dotProduct(v: Point) {
+        return this.x * v.x + this.y * v.y + this.z * v.z;
+    }
+
+    crossProduct(v: Point) {
+        const x = this.y * v.z - this.z * v.y;
+        const y = this.z * v.x - this.x * v.z;
+        const z = this.x * v.y - this.y * v.x;
+        return new Point(x, y, z);
+    }
+
+    unit() {
+        const mod = (this.x ** 2 + this.y ** 2 + this.z ** 2) ** (1 / 2);
+        const x = this.x / mod;
+        const y = this.y / mod;
+        const z = this.z / mod;
+        return new Point(x, y, z);
+    }
 }
 
 class Triangle {
@@ -11,12 +62,22 @@ class Triangle {
 
 class Camera {
     private position: Point;
-    private normal: Point;
+    private normal: Point; // Normal vector of screen plane
+    private i: Point; // Unit vector of screen plane x axis
+    private j: Point; // Unit vector of screen plane y axis
+    private screenDistance: number;
     private objects: Array<Triangle>;
 
-    constructor() {
+    constructor(
+        screenDistance: number = 50,
+        normal: Point = new Point(0, 0, 1),
+        j: Point = new Point(0, 1, 0),
+    ) {
         this.position = new Point(0, 0, 0);
-        this.normal = new Point(0, 0, 20);
+        this.normal = normal.unit();
+        this.j = j.unit();
+        this.i = this.normal.crossProduct(this.j);
+        this.screenDistance = screenDistance;
         this.objects = [];
     }
 
@@ -24,10 +85,26 @@ class Camera {
         this.objects.push(object);
     }
 
-    public draw() {
-        const objectsOnScreen = this.projectObjectsOnScreen();
+    public translate(dx: number = 0, dy: number = 0, dz: number = 0) {
+        this.position = new Point(
+            this.position.x + dx,
+            this.position.y + dy,
+            this.position.z + dz,
+        );
+    }
 
-        for (const object of objectsOnScreen) {
+    public rotate(axis: "x" | "y" | "z", angle: number) {
+        this.normal = this.normal.rotate(axis, angle);
+        this.j = this.j.rotate(axis, angle);
+        this.i = this.normal.crossProduct(this.j);
+    }
+
+    public draw() {
+        const objectsOnScreen = this.projectObjectsOnScreen(); // Objects projected on screen but on global coordinates
+        const objectsOnScreenCoordinates =
+            this.objectsToScreenPlaneCoordinates(objectsOnScreen); // Objects on screen's x, y coordinates
+
+        for (const object of objectsOnScreenCoordinates) {
             Display.stroke(object);
         }
     }
@@ -52,18 +129,20 @@ class Camera {
             projectionVertices.push(projectionVertex);
         }
 
-        console.log(object.vertices);
-        console.log(projectionVertices);
-
         return new Triangle(projectionVertices, object.color);
     }
 
     private projectPointOnScreen(point: Point) {
+        const {
+            x: xScreenOrigin,
+            y: yScreenOrigin,
+            z: zScreenOrigin,
+        } = this.normal.scale(this.screenDistance);
         const { x: a, y: b, z: c } = this.normal;
         const d =
-            this.normal.x * (this.position.x + this.normal.x) +
-            this.normal.y * (this.position.y + this.normal.y) +
-            this.normal.z * (this.position.z + this.normal.z);
+            this.normal.x * (this.position.x + xScreenOrigin) +
+            this.normal.y * (this.position.y + yScreenOrigin) +
+            this.normal.z * (this.position.z + zScreenOrigin);
 
         const alfaX = point.x - this.position.x;
         const alfaY = point.y - this.position.y;
@@ -76,11 +155,51 @@ class Camera {
                 c * this.position.z) /
             (a * alfaX + b * alfaY + c * alfaZ);
 
-        const projectionX = this.position.x + alfaX * t;
-        const projectionY = this.position.y + alfaY * t;
-        const projectionZ = this.position.z + alfaZ * t;
+        const xProjection = this.position.x + alfaX * t;
+        const yProjection = this.position.y + alfaY * t;
+        const zProjection = this.position.z + alfaZ * t;
 
-        return new Point(projectionX, projectionY, projectionZ);
+        return new Point(xProjection, yProjection, zProjection);
+    }
+
+    private objectsToScreenPlaneCoordinates(objects: Array<Triangle>) {
+        const objectsInScreenPlaneCoordinates: Array<Triangle> = [];
+
+        for (const object of objects) {
+            const objectInScreenPlaneCoordinates =
+                this.objectToScreenPlaneCoordinates(object);
+            objectsInScreenPlaneCoordinates.push(
+                objectInScreenPlaneCoordinates,
+            );
+        }
+
+        return objectsInScreenPlaneCoordinates;
+    }
+
+    private objectToScreenPlaneCoordinates(object: Triangle) {
+        const verticesInScreenPlaneCoordinates: Array<Point> = [];
+
+        for (const vertex of object.vertices) {
+            const vertexInScreenPlaneCoordinates =
+                this.toScreenPlaneCoordinates(vertex);
+            verticesInScreenPlaneCoordinates.push(
+                vertexInScreenPlaneCoordinates,
+            );
+        }
+
+        return new Triangle(verticesInScreenPlaneCoordinates, object.color);
+    }
+
+    private toScreenPlaneCoordinates(point: Point) {
+        const screenOrigin = this.position.add(
+            this.normal.scale(this.screenDistance),
+        );
+        const p = screenOrigin.scale(-1).add(point);
+
+        const x = p.dotProduct(this.i);
+        const y = p.dotProduct(this.j);
+
+        return new Point(x, y, 0); // Arbitrary z value, as canvas coordinates only have x and y
     }
 }
 
@@ -100,31 +219,30 @@ class Display {
 
     static stroke(object: Triangle) {
         this.drawAxis();
-
         const c = Display.c;
-        const screenObject = this.toScreenCoordinates(object);
-        c.strokeStyle = screenObject.color;
+        const canvasObject = this.toCanvasCoordinates(object);
+        c.strokeStyle = canvasObject.color;
         c.beginPath();
-        c.moveTo(screenObject.vertices[0].x, screenObject.vertices[0].y);
-        c.lineTo(screenObject.vertices[1].x, screenObject.vertices[1].y);
-        c.lineTo(screenObject.vertices[2].x, screenObject.vertices[2].y);
-        c.lineTo(screenObject.vertices[0].x, screenObject.vertices[0].y);
+        c.moveTo(canvasObject.vertices[0].x, canvasObject.vertices[0].y);
+        c.lineTo(canvasObject.vertices[1].x, canvasObject.vertices[1].y);
+        c.lineTo(canvasObject.vertices[2].x, canvasObject.vertices[2].y);
+        c.lineTo(canvasObject.vertices[0].x, canvasObject.vertices[0].y);
         c.stroke();
     }
 
-    // Assuming that the object received is already on the screen plane
-    private static toScreenCoordinates(object: Triangle) {
+    private static toCanvasCoordinates(object: Triangle) {
+        // object must be on the screen plane
         const { width, height } = Display.screen;
-        const verticesOnScreenCoordinates: Array<Point> = [];
+        const verticesOnCanvasCoordinates: Array<Point> = [];
 
         for (const vertex of object.vertices) {
             const x = width / 2 + vertex.x;
             const y = height / 2 - vertex.y;
-            const point = new Point(x, y, 0); // Arbitrary z value, as screen coordinates only have x and y
-            verticesOnScreenCoordinates.push(point);
+            const point = new Point(x, y, 0); // Arbitrary z value, as canvas coordinates only have x and y
+            verticesOnCanvasCoordinates.push(point);
         }
 
-        return new Triangle(verticesOnScreenCoordinates, object.color);
+        return new Triangle(verticesOnCanvasCoordinates, object.color);
     }
 
     private static drawAxis() {
@@ -147,11 +265,15 @@ class Display {
 
 (function t() {
     const triangle = new Triangle([
-        new Point(0, 200, 75),
+        new Point(0, 200, 100),
         new Point(200, 200, 100),
         new Point(200, 0, 100),
     ]);
-    const camera = new Camera();
+    const camera = new Camera(50, new Point(0, 0, 1), new Point(0, 1, 0));
     camera.addObject(triangle);
     camera.draw();
+    setTimeout(() => {
+        camera.rotate("y", 90);
+        camera.draw();
+    }, 1000);
 })();
