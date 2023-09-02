61fe5147ebd88199004375fd17c32454da96fb71
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-09-02 15:58:07 -0300

    fix: add tsc --watch alternative because of add-extensions.js

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { Camera } from "./classes/camera";
 import { InputHandler } from "./classes/input-handler";
 import { cube } from "./objects/cube";
 
-let x = 'from "./classes/point"';
-
 (function start() {
     const camera = new Camera(200, new Point(0, 0, 1), new Point(0, 1, 0));
     for (const triangle of cube) {
